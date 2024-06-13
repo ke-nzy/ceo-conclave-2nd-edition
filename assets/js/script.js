3a3a1621a82c77e0c099e1c571fe4a1ed91fb3fa
Kennedy Ngugi <ngugikennedy4@gmail.com>
2024-06-13 17:22:30 +0300

    ceo conclave 2nd edition landing page

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+document.getElementById('mobile-menu').addEventListener('click', function () {
+    const sidebar = document.getElementById('sidebar');
+    if (sidebar.style.left === '0px') {
+        sidebar.style.left = '-250px';
+    } else {
+        sidebar.style.left = '0px';
+    }
+});
