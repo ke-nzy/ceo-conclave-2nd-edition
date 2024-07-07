a712f2fc09aa0585fcd4fbf2452e5755b756a934
Kennedy Ngugi <ngugikennedy4@gmail.com>
2024-07-07 15:58:25 +0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -94,3 +94,14 @@ window.addEventListener("scroll", function () {
     }
   }
 });
+
+// Disable right-click context menu
+document.addEventListener('contextmenu', function(e) {
+  e.preventDefault();
+});
+
+document.addEventListener('keydown', function(e) {
+  if (e.key === 'F12' || (e.ctrlKey && e.shiftKey && (e.key === 'I' || e.key === 'J')) || (e.ctrlKey && e.key === 'U')) {
+      e.preventDefault();
+  }
+});
